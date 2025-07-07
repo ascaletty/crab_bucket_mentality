5f32398b35a5a6176b17e3aff51f94e2d2af9e7a
Aaron Scaletty <ascaletty23@gmail.com>
2025-07-06 23:45:35 -0500

    added working cache and things. Todo: sorting based on how many times the program has been used

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+
 use gtk::{
     Application, ApplicationWindow, Button, ListBox, PolicyType, ScrolledWindow, TextView, glib,
 };
 use gtk::{Window, prelude::*};
-
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 use std::collections::HashMap;
 use std::os::unix::process::CommandExt;
@@ -10,7 +10,6 @@ use std::process::Command;
 #[path = "program_paths.rs"]
 mod path;
 fn main() -> glib::ExitCode {
-    path::parse_programs();
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
@@ -21,17 +20,25 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 
-fn build_ui(app: &Application) {
-    let mut programs = HashMap::new();
-    programs = path::read_cache(programs).expect("failed to parse csv");
+fn build_ui(app: &Application) { 
+    let mut programs_vec: Vec<path::ProgramData> = Vec::new();
+    programs_vec=path::parse_programs(programs_vec).expect("failed to parse program");
     let program_list = ListBox::new();
-    for (program, exec) in programs {
-        let label = Button::with_label(&program);
+    for program_data in programs_vec {
+        let label = Button::with_label(program_data.name.as_str());
+        if program_data.terminal == true{
+         label.connect_clicked(move |label| {
+            let mut temp = Command::new("kitty");
+            let mut cmd= temp.args([program_data.exec.as_str()]);
+            cmd.exec();
+         });
+        }
+        else{
         label.connect_clicked(move |label| {
-            label.set_label("hellogheran");
-            let mut cmd = Command::new(&exec);
+            let mut cmd = Command::new(program_data.exec.as_str());
             cmd.exec();
         });
+        }
         program_list.append(&label);
     }
     let scrolled_window = ScrolledWindow::builder()
@@ -42,7 +49,8 @@ fn build_ui(app: &Application) {
 
     let text_input = TextView::builder()
         .name("drun")
-        .receives_default(true)
+        .top_margin(300)
+        .bottom_margin(300)
         // .opacity(50)
         .build();
     let window = Window::builder()
@@ -60,3 +68,4 @@ fn build_ui(app: &Application) {
     // Present window
     window.present();
 }
+
--- src/program_paths.rs
+++ src/program_paths.rs
@@ -2,47 +2,77 @@ use std::env;
 use std::error;
 use std::fs;
 use std::fs::exists;
+use std::process::Termination;
 // use std::os::unix::process::CommandExt;
 use freedesktop_entry_parser::parse_entry;
 use std::path::Path;
 use std::process::Command;
-use std::{collections::HashMap, fs::File};
+use std::{collections::HashMap, error::Error, fs::File, io, process};
+pub struct ProgramData {
+    pub name: String,
+    pub exec: String,
+    pub terminal: bool,
+    pub no_display: bool,
+}
 
 fn parse_desktop(program: &str) -> freedesktop_entry_parser::Entry {
     let path = Path::new(program);
-    // let contents = fs::read_to_string(&path).expect("unable to read .desktop");
+    println!("parsing {program}");
     let program_data = parse_entry(path).expect("failed to parse Desktop Entry");
-    let start_cmd = program_data
-        .section("Desktop Entry")
-        .attr("Exec")
-        .expect("Attribute doesn't exist");
-    println!("{}", start_cmd);
     program_data
 }
-fn create_cache(program_names: String) {
-    let mut program_data_vec: Vec<freedesktop_entry_parser::Entry> = vec![];
+fn create_cache(program_names: String, path: &Path) {
+    // let mut program_data_vec: Vec<freedesktop_entry_parser::Entry> = vec![];
+    let mut wtr = csv::Writer::from_path(path).expect("writer gen failed");
     for line in program_names.lines() {
-        println!("parsing {line}");
-        program_data_vec.push(parse_desktop(line));
+        println!("creating cache for {line}");
+        wtr.write_record(&[
+            "0",
+            parse_desktop(line)
+                .section("Desktop Entry")
+                .attr("Name")
+                .expect("attribute does not exist"),
+            parse_desktop(line)
+                .section("Desktop Entry")
+                .attr("Exec")
+                .expect("attribute does not exist"),
+            parse_desktop(line)
+                .section("Desktop Entry")
+                .attr("Terminal")
+                .unwrap_or("false"),
+            parse_desktop(line)
+                .section("Desktop Entry")
+                .attr("NoDisplay")
+                .unwrap_or("false"),
+        ])
+        .expect("writing to csv failed");
     }
 }
-fn check_for_match(input: String) -> bool {
-    let cache_dir = Path::new("/home/ascaletty23/.cache/crab_bucket_mentality.txt");
-    let iscache_exist = cache_dir.exists();
-    if iscache_exist == true {
-        let contents = fs::read_to_string(&cache_dir).expect("unable to read .desktop");
-        if input == contents {
-            return true;
-        } else {
-            fs::write(cache_dir, input);
-            return false;
-        }
+fn check_for_updates(input: String) {
+    let program_list_raw = Path::new("/home/ascaletty23/.cache/crab_bucket_mentality.txt");
+    let program_list_raw_exist = program_list_raw.exists();
+    let cache_dir = Path::new("/home/ascaletty23/.cache/crab_bucket_mentality.cache");
+    let cache_dir_exist = cache_dir.exists();
+    if cache_dir_exist != true {
+        fs::write(program_list_raw, &input);
+        create_cache(input, cache_dir);
+        println!("cache does not exist, writing program list and cache");
     } else {
-        fs::write(cache_dir, input);
-        return true;
+        if program_list_raw_exist == true {
+            let contents = fs::read_to_string(&program_list_raw).expect("unable to read .desktop");
+            if input == contents {
+                println!("program_list unchanged");
+            } else {
+                fs::write(program_list_raw, &input);
+                create_cache(input, cache_dir);
+                println!("program list changed and cache rewritten");
+            }
+        } else {
+            println!("error occured in checking for updates");
+        }
     }
 }
-pub fn parse_programs() {
+pub fn parse_programs(programs: Vec<ProgramData>) -> Result<Vec<ProgramData>, Box<dyn Error>> {
     let applications = Path::new("/usr/share/applications/");
     assert!(env::set_current_dir(&applications).is_ok());
     let output = Command::new("sh")
@@ -51,19 +81,32 @@ pub fn parse_programs() {
         .output()
         .expect("failed to execute process");
     let program_names = String::from_utf8_lossy(&output.stdout);
-    if check_for_match(program_names.to_string()) == true {
-        print!("cache exist and unchanged");
-    } else {
-    }
+    check_for_updates(program_names.to_string());
+    read_cache(programs)
 }
-pub fn read_cache(
-    mut programs: HashMap<String, String>,
-) -> Result<HashMap<String, String>, Box<dyn error::Error>> {
-    let file = File::open("/home/ascaletty23/.cache/kaolinitedrun.cache")?;
-    let mut rdr = csv::Reader::from_reader(file);
+fn read_cache(mut programs: Vec<ProgramData>) -> Result<Vec<ProgramData>, Box<dyn error::Error>> {
+    let file = Path::new("/home/ascaletty23/.cache/crab_bucket_mentality.cache");
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(file)
+        .expect("writer gen failed");
     for result in rdr.records() {
         let record = result?;
-        programs.insert(record[0].to_string(), record[1].to_string());
+        println!("reading {:?}", record);
+        let mut program_data = {
+            ProgramData {
+                name: "none".to_string(),
+                exec: "none".to_string(),
+                terminal: false,
+                no_display: false,
+            }
+        };
+        program_data.name = record[1].to_string();
+        let trimming_exec: Vec<&str> = record[2].split(" ").collect();
+        program_data.exec = trimming_exec[0].to_string();
+        program_data.terminal = record[3].parse().unwrap_or(false);
+        program_data.no_display = record[4].parse().unwrap_or(false);
+        programs.push(program_data);
     }
     Ok(programs)
 }
